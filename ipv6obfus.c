2a00bcb82cf006d2eda6e6b33e21d50b3750b80b
Komando <Karl@DESKTOP-OT4GJSI>
2024-07-25 03:17:00 +0300

    Added comments

--- ipv6obfus.c
+++ ipv6obfus.c
@@ -148,6 +148,7 @@ int main(void) {
 		GenerateIpv6Output(rawData, sizeof(rawData));
 	}
 
+	// Freeing PaddedBuffer if needed
 	if (PaddedBuffer != NULL) {
 		HeapFree(GetProcessHeap(), 0, PaddedBuffer);
 	}
